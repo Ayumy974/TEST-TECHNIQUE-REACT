b03f626dbc5892e45f80f6c6d67d1e503c8879c8
fklock <francoiseklock@gmail.com>
2020-10-23 11:32:06 +0200

    Install react-router-dom package and created the router component in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <h1>Ma petite application répertoriant des utilisateurs</h1>
-    </div>
+    </Router>
   );
 }
 
