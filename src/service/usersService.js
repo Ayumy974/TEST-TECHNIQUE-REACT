29a9d5645e86ad4c7c71c365d46a1177308a29ef
fklock <francoiseklock@gmail.com>
2020-10-23 12:37:28 +0200

    installation of the axios library + creation of a UsersService class to centralise all axios requests

---
+++ src/service/usersService.js
@@ -0,0 +1,18 @@
+// *** Requêtes vers le serveur REST ***
+// Avec l'usage de la méthode static, nous n'avons pas à instancier notre service et nous pouvons utiliser la classe  directement
+
+// Utilisation de la librairie axios pour effectuer les requêtes vers l'API rest
+import axios from 'axios';
+
+export default class UsersService {
+
+    static getMovie() {
+        return axios({
+            'method': 'GET',
+            'url': `http://localhost:5000/users`,
+        })
+        .then(response => response.data)
+        .catch(error => console.log(error))
+    }
+    
+}
